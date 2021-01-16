eb8de7a34d60f957d4deffd1657b0ff3c36381fa
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-01-16 11:10:30 -0500

    fixed styles

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { TextBreakdown } from './TextBreakdown';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsarcade.textBreakdown', () => {
-		TextBreakdown.createOrShow(context.extensionUri);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('vsarcade.textBreakdown', () => {
 
-		const currentEditor = vscode.window.activeTextEditor;
-		let code = [];
+			TextBreakdown.createOrShow(context.extensionUri);
 
-		if (currentEditor) {
-			const document = currentEditor.document;
-			
-			const endLine = document.lineCount;
-			for (let i = 0; i < endLine; i++) {
-				code.push(document.lineAt(i).text);
+			const currentEditor = vscode.window.activeTextEditor;
+			let code = [];
+
+			if (currentEditor) {
+				const document = currentEditor.document;
+				
+				const endLine = document.lineCount;
+				for (let i = 0; i < endLine; i++) {
+					code.push(document.lineAt(i).text);
+				}
 			}
-		}
 
-		TextBreakdown.currentPanel?.refactor({code});
+			TextBreakdown.currentPanel?.refactor({code});
+			// TextBreakdown.currentPanel?.refactor(
+			// 	{
+			// 		styles: {
+			// 			background: 'red'
+			// 		}
+			// 	}
+			// );
 
-	});
+		})
+	);
 
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
