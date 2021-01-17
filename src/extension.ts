44b05a67a6b9513b4d350cde1534b49a8de01fe0
Eric Huang <eric.haoran.huang@gmail.com>
2021-01-17 08:11:26 -0500

    Version 0.5.0 ready

--- src/extension.ts
+++ src/extension.ts
@@ -24,6 +24,12 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vsarcade.textBreakdown', () => {
 
+			const currentEditor = vscode.window.activeTextEditor;
+			if(!currentEditor) {
+				vscode.window.showInformationMessage("Must have an active text editor to play VS Arcade Textbreakdown!");
+				return;
+			}
+
 			TextBreakdown.createOrShow(context.extensionUri);
 
 			let code = getCode();
@@ -44,6 +50,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand('vsarcade.balldrop', () => {
+
+			vscode.window.showInformationMessage("This command is still in its beta, non-functional screen");
+			return;
+
 			BallDrop.createOrShow(context.extensionUri);
 
 			let code = getCode();
